7de07fe4d3fef924058372c5cc42ad2a6358556c
Marcelo Henrique <marcelohs_93@hotmail.com>
2020-05-05 22:56:49 +1000

    primeiro projeto com react, configuracao e estilizacao

---
+++ primeiro-projeto-react/src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositorios no GitHub</Title>;
+};
+
+export default Dashboard;
